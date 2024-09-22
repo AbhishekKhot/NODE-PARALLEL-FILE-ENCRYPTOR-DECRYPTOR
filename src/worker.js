db8fed3656fea4d56174131d7dae2cbae91565bc
abhishekkhot <abhishek.khot@idfy.com>
2024-09-22 08:44:27 +0530

    added worker threads

---
+++ src/worker.js
@@ -0,0 +1,50 @@
+const { parentPort, workerData, threadId } = require("worker_threads");
+const { ACTIONS } = require("./lib/constant");
+const fs = require("fs");
+const Cryption = require("./cryption");
+const AES = require("./strategies/aes");
+const DES = require("./strategies/des");
+const RSA = require("./strategies/rsa");
+
+async function processFiles({
+  action,
+  algorithm,
+  inputFilePath,
+  outputFilePath,
+}) {
+  const strategy = getStrategy(algorithm);
+  const cryption = new Cryption(strategy);
+  try {
+    if (action === ACTIONS.ENCRYPT) {
+      await cryption.encrypt(inputFilePath, outputFilePath);
+      parentPort.postMessage(
+        `Thread ID: ${threadId}, Action: ${action}, File: ${inputFilePath}`
+      );
+    } else if (action === ACTIONS.DECRYPT) {
+      await cryption.decrypt(inputFilePath, outputFilePath);
+      parentPort.postMessage(
+        `Worker ID: ${threadId}, Action: ${action}, File: ${inputFilePath}`
+      );
+    }
+    await fs.promises.unlink(inputFilePath);
+  } catch (error) {
+    parentPort.postMessage(
+      `Error processing file ${inputFilePath}: ${error.message}`
+    );
+  }
+}
+
+function getStrategy(algorithm) {
+  switch (algorithm) {
+    case "AES":
+      return new AES();
+    case "DES":
+      return new DES();
+    case "RSA":
+      return new RSA();
+    default:
+      return;
+  }
+}
+
+processFiles(workerData);
